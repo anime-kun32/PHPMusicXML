f0d3d3040a798790d9ddf9f0dc842d2c4ccc2845
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-05-13 14:17:21 +0100

    Update MusicXML.php

--- inc.lib/classes/MusicXML/MusicXML.php
+++ inc.lib/classes/MusicXML/MusicXML.php
@@ -8,27 +8,58 @@ use MusicXML\Map\NodeType;
 
 class MusicXML extends MusicXMLBase
 {
+    /**
+     * @var mixed Holds the loaded MusicXML object.
+     */
+    private $musicObject;
+
+    /**
+     * Loads a MusicXML file and creates the corresponding object.
+     *
+     * @param string $path Path to the XML file.
+     * @return void
+     */
     public function loadXml($path)
     {
         $domdoc = new DOMDocument();
         $domdoc->loadXML(file_get_contents($path));
         $nodes = $domdoc->childNodes;
-        $object = null;
-        foreach($nodes as $node)
-        {
-            if($node->nodeType == NodeType::ELEMENT && isset(ModelMap::CLASS_MAP[$node->tagName])) //NOSONAR
-            {
-                $className = ModelMap::CLASS_MAP[$node->tagName]; //NOSONAR
-                $object = new $className($node);
+
+        foreach ($nodes as $node) {
+            if ($node->nodeType == NodeType::ELEMENT && isset(ModelMap::CLASS_MAP[$node->tagName])) {
+                $className = ModelMap::CLASS_MAP[$node->tagName];
+                $this->musicObject = new $className($node);
                 break;
             }
         }
-        echo $object;  
-        
+
+        // Optionally: check if loaded successfully
+        if (!$this->musicObject) {
+            throw new \Exception("Failed to parse MusicXML file or unsupported root tag.");
+        }
     }
 
-    
-    
+    /**
+     * Returns the loaded MusicXML object.
+     *
+     * @return mixed
+     */
+    public function getMusicXml()
+    {
+        return $this->musicObject;
+    }
 
-    
+    /**
+     * Optionally: Converts the object back to XML string if the object supports it.
+     *
+     * @return string|null
+     */
+    public function exportXml()
+    {
+        if (method_exists($this->musicObject, 'toXml')) {
+            return $this->musicObject->toXml();
+        }
+
+        return null;
+    }
 }
